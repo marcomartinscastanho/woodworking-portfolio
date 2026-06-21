55f7f41f74f4c372ee04c2fb9b4b936af3c1d5c9
Marco Castanho <marco.castanho@gmail.com>
2026-06-21 12:06:16 +0100

    auto play carousel

--- components/ProjectCarousel.tsx
+++ components/ProjectCarousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 
@@ -9,18 +10,31 @@ type Props = {
 };
 
 export default function ProjectCarousel({ images, title }: Props) {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: false,
+    },
+    [
+      Autoplay({
+        delay: 5000,
+        stopOnInteraction: false,
+        stopOnMouseEnter: true,
+      }),
+    ],
+  );
 
   return (
-    <div className="overflow-hidden rounded-xl" ref={emblaRef}>
-      <div className="flex">
-        {images.map((image, index) => (
-          <div key={index} className="min-w-full">
-            <div className="relative aspect-3/4 md:aspect-4/3 ">
-              <Image src={image} alt={`${title} image ${index + 1}`} fill className="object-cover" />
+    <div className="embla">
+      <div className="overflow-hidden rounded-xl" ref={emblaRef}>
+        <div className="flex">
+          {images.map((image, index) => (
+            <div key={index} className="min-w-full">
+              <div className="relative aspect-3/4 md:aspect-4/3 ">
+                <Image src={image} alt={`${title} image ${index + 1}`} fill className="object-cover" />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
