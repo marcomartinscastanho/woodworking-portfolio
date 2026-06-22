73272bd32e910c83dc3b98fe09f3e01af552767d
Marco Castanho <marco.castanho@gmail.com>
2026-06-22 11:22:58 +0100

    no bots

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="border-t mt-12">
-      <div className="mx-auto max-w-6xl p-4 flex flex-wrap gap-4">
-        <Link href="/about">About Me</Link>
-        <a href="https://github.com/marcomartinscastanho" target="_blank">
-          GitHub
-        </a>
-        <a href="https://linkedin.com/in/marcomcastanho" target="_blank">
-          LinkedIn
-        </a>
+      <div className="mx-auto max-w-6xl p-4 flex justify-between">
+        <div>
+          <p className=" text-gray-500 text-sm">
+            &copy; {new Date().getFullYear()} Marco Martins Castanho. All rights reserved.
+          </p>
+          <p className="text-gray-500 text-xs">
+            All photographs and project images are copyrighted and may not be reproduced or used for AI training without
+            permission.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-4">
+          <Link href="/about">About Me</Link>
+          <a href="https://github.com/marcomartinscastanho" target="_blank">
+            GitHub
+          </a>
+          <a href="https://linkedin.com/in/marcomcastanho" target="_blank">
+            LinkedIn
+          </a>
+        </div>
       </div>
     </footer>
   );
